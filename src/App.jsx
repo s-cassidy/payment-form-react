b8c4c526528b69ea32c31c4d99863e5919ebca89
Sam Cassidy <sam@cassidys.email>
2023-12-21 19:04:54 +0000

    Add flex styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-cyan-300 font-extrabold">Pay now. Regret later</h1>
-      <form >
+      <form className='flex flex-col w-1/3'>
         <CardNo />
         <CardType />
       </form>

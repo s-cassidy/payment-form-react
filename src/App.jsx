91a832e75214de65317ee984a1462cee3e7cb19f
Sam Cassidy <sam@cassidys.email>
2023-12-21 19:03:24 +0000

    Add CardType component
    Add CardType to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { CardNo } from './components/CardNo'
+import { CardType } from './components/CardType'
 
 function App() {
 
   return (
     <>
       <h1 className="text-cyan-300 font-extrabold">Pay now. Regret later</h1>
-      <form>
+      <form >
         <CardNo />
+        <CardType />
       </form>
     </>
   )
---
+++ src/components/CardType.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+export function CardType() {
+  const [cardType, setCardType] = useState('')
+
+  return (
+    <>
+      <label className='text-gray-200 font-bold' htmlFor='cardName'>Card type</label>
+      <select
+        name="cardType"
+        id="cardType"
+        type=''
+        value={cardType}
+        onChange={(e) => setCardType(e.target.value)}
+        className='border-cyan-300 border-solid border-2 rounded'
+      >
+        <option value='visa'>Visa</option>
+        <option value='visa'>American Express</option>
+        <option value='visa'>MasterCard</option>
+      </select>
+    </>
+
+  )
+}
